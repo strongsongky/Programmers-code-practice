9fb3d4dc798dcbec309af79bd676ee4b4da2e4fa
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-11-02 19:21:52 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = [];
+    var result = s.split(" ")
+    
+    for (let i = 0; i < result.length; i++){
+        let sum = '';
+        for(let j = 0; j<result[i].length;j++){
+            if (j%2 === 0){
+                sum += result[i][j].toUpperCase()
+            }  else {
+                sum += result[i][j].toLowerCase()
+            }
+        }
+        answer.push(sum)
+    }
+    return answer.join(' ')
+}
