78580daec867c0c2b043cfd5d7a1e9d8ea736266
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-12-04 17:19:55 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,11 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    
+    for(let i = 0;  i < n; i++){
+        let num = (arr1[i] | arr2[i]).toString(2);
+        num = num.replace(/1/g, "#").replace(/0/g, " ");
+        answer.push(num.padStart(n, " "));
+    }
+    
+    return answer;
+}
