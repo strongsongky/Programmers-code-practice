66bbdc69558a31577398335107500aed467639ef
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-12-15 23:59:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.78 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,11 @@
+function solution(A,B){
+  var answer = []
+  var a = A.sort((a,b) => a-b)
+  var b = B.sort((a,b) => b-a)
+  for(var i=0; i<a.length; i++){
+    answer.push(a[i]*b[i])
+  }
+  
+  var result = answer.reduce((a,b) => a+b)
+  return result
+}
