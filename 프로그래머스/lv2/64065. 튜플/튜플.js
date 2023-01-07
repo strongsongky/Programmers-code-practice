ffcbe352df25309f4c9b19eac6227539f4f8e4cc
strongsongky <102295416+strongsongky@users.noreply.github.com>
2023-01-07 16:21:00 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    return JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
+    .sort((a, b) => a.length - b.length)
+    .reduce((arr, v, n) => {
+        if (n) {
+            return arr.concat(v.filter(f => !arr.includes(f)));
+        }
+        return v;
+    }, []);
+}
