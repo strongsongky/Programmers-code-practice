5b9ca0b80633bb88c748584367c684abb9a1d6c7
Dev Song <102295416+strongsongky@users.noreply.github.com>
2023-08-08 14:55:46 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.c
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -3,21 +3,8 @@
 #include <stdlib.h>
 
 int solution(int num1, int num2) {
-    int answer = (num1 == num2) ? 1 : -1;
-    return answer;
-}
+    int answer = 0;
 
-int main() {
-    int num1 = 5;
-    int num2 = 5;
-    
-    int result = solution(num1, num2);
-    
-    if (result == 1) {
-        printf("num1 and num2 are equal.\n");
-    } else if (result == -1) {
-        printf("num1 and num2 are different.\n");
-    }
-    
-    return 0;
+
+    return num1 == num2? 1: -1;
 }
