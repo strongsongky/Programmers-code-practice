0b037ac4333184d240ccfa301c319fef7d6dfd17
Dev Song <102295416+strongsongky@users.noreply.github.com>
2024-01-22 16:42:47 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.16 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(int n, const char* control) {
+    int len = strlen(control);
+
+    for (int i = 0; i < len; i++) {
+        switch (control[i]) {
+            case 'w':
+                n += 1;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return n;
+}
